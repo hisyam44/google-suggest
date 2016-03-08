1c67d69a0e944b782f377fb6b942c82b558f764a
hisyam <mohammadhisyam4@gmail.com>
2016-03-08 18:39:31 +0700

    Version 1

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
   <title>Search Suggestion</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="container">
@@ -17,25 +17,33 @@
 				<div class="panel-body">
 					<form class="form">
 						<div class="form-group">
-							<input class="form-control" name="q" placeholder="Cari Disini...">
+							<input class="form-control" name="q" placeholder="Kata Kunci...">
+						</div>
+						<div class="form-group">
+							<button class="btn btn-primary">Cari</button>
 						</div>
 					</form>
 					<ul class="list-group">
 					<?php
 					if(isset($_GET['q'])){
-						$query = $_GET['q'];
-						$url = 'http://clients1.google.co.id/complete/search?hl=id&output=toolbar&q='.$query;
-						$xml = simplexml_load_file($url) or die("feed not loading");
-						$data = [];
-						$str = '';
-						$suggests = $xml->CompleteSuggestion;
-						foreach ($suggests as $suggest) {
-							echo "<li class='list-group-item'>".$suggest->suggestion['data']."</li>";
-							array_push($data, $str.$suggest->suggestion['data']);
-							foreach($suggest->suggestion->attributes() as $attribute){
+						function suggest(){
+							$query = $_GET['q'];
+							$url = 'https://clients1.google.co.id/complete/search?hl=id&output=toolbar&q='.$query;
+							$xml = simplexml_load_file($url) or die("feed not loading");
+							$data = [];
+							$str_data = '';
+							$str = '';
+							$suggests = $xml->CompleteSuggestion;
+							foreach ($suggests as $suggest) {
+								$str_data .= "<li class='list-group-item'>".$suggest->suggestion['data']."</li>";
+								echo "<li class='list-group-item'>".$suggest->suggestion['data']."</li>";
+								array_push($data, $str.$suggest->suggestion['data']);
+								foreach($suggest->suggestion->attributes() as $attribute){
+								}
 							}
+							$results = array('results' => $data );
 						}
-						$results = array('results' => $data );
+						suggest();
 					}
 					?>
 					</ul>
